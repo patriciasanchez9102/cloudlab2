1a0da72f5ea1a6c1ee6dbdc6550d7890b31d9328
Ana Patricia Sanchez <patriciasanchez813@gmail.com>
2019-07-20 12:26:04 +0200

    event controller

--- src/main/java/hello/controller/EventsController.java
+++ src/main/java/hello/controller/EventsController.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
+
+
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +25,34 @@ public class EventsController {
     public List<Event> fetchEvents() {
         return eventsRepository.findAll();
     }
-
+    @GetMapping("/event/by/{id}")
+    public Event byId(@PathVariable long id) {
+   	 return eventsRepository.findById(id).get();
+   }
+    
+    @PostMapping("/event/new")
+    public List<Event> addEvent(@RequestBody Event e ){
+    	eventsRepository.save(e);
+    	return eventsRepository.findAll();
+    }
+    
+    @PutMapping("/event/put/{id}")
+    public List<Event> putEvent(@RequestBody Event e,@PathVariable Long id){
+    	Optional<Event> list=eventsRepository.findById(id);
+    	if(!list.isPresent()) {
+			throw new EventNotFoundException();
+		}
+    	list.get().setId(e.getId());
+    	list.get().setName(e.getName());
+    	eventsRepository.save(list.get());
+    	return eventsRepository.findAll();
+    }
+    
+    @DeleteMapping("/event/delete/{id}")
+    public List<Event> deleteEvent(@PathVariable Long id){
+    	eventsRepository.deleteById(id);
+    	return eventsRepository.findAll();
+    }
+    
 }
 
